254c3e89c12ac87e3475b66f9cee27f1a54e8cd6
Akeymurth <arkeyto@gmail.com>
2024-06-29 10:37:43 -0600

    addMenuMobile

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', ()=>{
     //esta linea lo que hace es borrar esta clase o ponerla
     //dentro de la clase desktop-menu para eso sirve el "toggle"
     desktopMenu.classList.toggle('inactive');
+});
+
+menuBurger.addEventListener('click', ()=>{
+    mobileMenu.classList.toggle('inactive');
 });
